bcdd8e39a0dab09f09a2a629f390715a3a48250c
SandeepSinghGahir <sandeizz@yahoo.com>
2019-01-06 02:26:38 -0800

    Added short solution but without using any builtin lib to Repeated Strings

--- RepeatedString.java
+++ RepeatedString.java
@@ -1,4 +1,21 @@
- // Complete the repeatedString function below.
+//Short Solution
+// Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+      return (occurancesofa(s,s.length()) * (n/s.length())) + occurancesofa(s,n%s.length());
+    }
+   
+    private static long occurancesofa(String s,long limit){
+      long occr = 0;
+      for(int i=0;i<limit;i++){
+      if(s.charAt(i)=='a')
+        occr++;   
+      }
+    return occr;
+    }
+
+
+//Long/Understandable Solution
+// Complete the repeatedString function below.
     static long repeatedString(String s, long n) {
     Solution sol = new Solution();
       long l = s.length();
@@ -10,11 +27,11 @@
     }
    
     private long occurancesofa(String s,long limit){
-    long occr = 0;
-    for(int i=0;i<limit;i++){
-    if(s.charAt(i)=='a')
-      occr++;   
-    }
+      long occr = 0;
+      for(int i=0;i<limit;i++){
+      if(s.charAt(i)=='a')
+        occr++;   
+      }
     return occr;
     }
 
